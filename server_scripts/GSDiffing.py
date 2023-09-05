3250c1b5ce13aaf21199c5f83d57ff31bfeee45d
jalvarez957 <87673004+KilloPillers@users.noreply.github.com>
2023-09-04 20:46:40 -0700

    Added scheduled script to sync MongoDB with Google spreadsheet

--- server_scripts/GSDiffing.py
+++ server_scripts/GSDiffing.py
@@ -1,13 +1,39 @@
 # when dokcerizing remember to install python and its dependancies
 # for this script
-from pymongo import MongoClient  # pip3 install pymongo
+from pymongo import MongoClient # pip3 install pymongo
+from gridfs import GridFS
 import gspread  # pip3 install gspread
 from oauth2client.service_account import ServiceAccountCredentials
 # pip3 install oath2client
 import csv
 import json
 import os
+import zlib
+import datetime
 
+def insert_review(review):
+    review["additional_comments"] = review["additional_comments"].strip('"')
+    review["difficulty"] = int(review["difficulty"])
+    review["like"] = 0
+    review["dislike"] = 0
+    #convert date from MM/DD/YYYY to YYYY/MM/DD
+    review["date"] = datetime.datetime.strptime(review["date"], "%m/%d/%Y").strftime("%Y/%m/%d")
+    query = {"class_name": review["class_name"],
+             "difficulty": int(review["difficulty"]),
+             "additional_comments": review["additional_comments"]}
+    #check to see if review already exists
+    if review_collection.find_one(query):
+        print("But review already exists in MongoDB")
+        return
+    result = review_collection.insert_one(review)
+    course = course_collection.find_one({"class_name": review["class_name"]})
+    avg_difficulty = round(float(course["average_diff"]), 2)
+    num_reviews = int(course["number_of_reviews"])
+    new_avg_difficulty = (avg_difficulty * num_reviews + int(review["difficulty"])) / (num_reviews + 1)
+    course_collection.update_one({"class_name": review["class_name"]},
+                                 {"$inc": {"number_of_reviews": 1},
+                                  "$set": {"average_diff": new_avg_difficulty}
+                                  })
 
 def download_new_sheet():
     scope = ['https://spreadsheets.google.com/feeds',
@@ -25,9 +51,13 @@ def download_new_sheet():
       credentials = ServiceAccountCredentials.from_json_keyfile_dict(credentials_data, scope)
       
     client = gspread.authorize(credentials)
-      
+     
+    # Official UCR Class Difficulty Spreadsheet
     spreadsheet = client.open_by_url(
             'https://docs.google.com/spreadsheets/d/1qiy_Oi8aFiPmL4QSTR3zHe74kmvc6e_159L1mAUUlU0/edit#gid=0')
+    # Test Spreadsheet
+    #spreadsheet = client.open_by_url(
+    #        'https://docs.google.com/spreadsheets/d/1ocQTvVgAA3MPMVLUkjZ8-PUX9LMGoO5ebA8A-NurO7w/edit?usp=sharing')
 
     worksheet = spreadsheet.worksheet('Sheet1')
 
@@ -42,16 +72,15 @@ def download_new_sheet():
 
 
 def compare_files():
-    old_sheet_path = './old.tsv'
     new_sheet_path = './new.tsv'
-
-    client = MongoClient(URI)
-    db = client['UCR']
-    collection = db['reviews']
+    fs = GridFS(db, collection='sysfiles')
+    compressed_file = fs.find_one({'filename': 'old.gz'})
+    old_sheet = zlib.decompress(compressed_file.read())
+    old_sheet = old_sheet.decode('utf-8')
+    needs_update = False
     try:
-        with open(old_sheet_path, 'r') as old_sheet, open(new_sheet_path, 'r')\
-                as new_sheet:
-            old_lines = [line.rstrip('\n') for line in old_sheet.readlines()]
+        with open(new_sheet_path, 'r') as new_sheet:
+            old_lines = [line.rstrip('\n') for line in old_sheet.splitlines()]
             new_lines = [line.rstrip('\n') for line in new_sheet.readlines()]
             i = 2
             j = 2
@@ -62,7 +91,6 @@ def compare_files():
                 if new_line_data[0] != '':
                     current_course = new_line_data[0]
                 if (old_lines[i] != new_lines[j]):
-                    print(j, new_line_data)
                     new_review = {"class_name": current_course,
                                   "additional_comments": new_line_data[2],
                                   "difficulty": new_line_data[3],
@@ -70,14 +98,17 @@ def compare_files():
                                   "like": 0,
                                   "dislike": 0
                                   }
-                    result = collection.insert_one(new_review)
-                    print("new review found:", result)
+                    print("New review found:", new_review)
+                    insert_review(new_review)
+                    needs_update = True
                     j += 1
                 else:
                     j += 1
                     i += 1
             # everything else is new by the pidgeonhole principle
             for new_line in new_lines[j:]:
+                if new_line != '':
+                    current_course = new_line[0]
                 new_line_data = new_line.split('\t')
                 new_review = {"class_name": current_course,
                               "additional_comments": new_line_data[2],
@@ -86,20 +117,43 @@ def compare_files():
                               "like": 0,
                               "dislike": 0
                               }
-                result = collection.insert_one(new_sheet)
-                print("new review found:", result)
+                print("New review found:", new_review)
+                insert_review(new_review)
+                needs_update = True
 
     except FileNotFoundError as error:
         print("File not found.", error)
     except IOError as error:
         print("An error occurred while reading the file.", error)
 
-    print("No more new reviews")
+    print("Finished comparing files")
 
-    os.remove('./old.tsv')
-    print("deleted old.tsv")
-    os.rename('./new.tsv', 'old.tsv')
-    print("renamed new.tsv to old.tsv")
+    file_path = "new.tsv"
+    compressed_file_path = "old.gz"
+
+    # Compress the file using zlib
+    with open(file_path, "rb") as source_file, open(compressed_file_path, "wb") as compressed_file:
+        data = source_file.read()
+        compressed_data = zlib.compress(data, level=zlib.Z_BEST_COMPRESSION)
+        compressed_file.write(compressed_data)
+    
+    if needs_update:
+        print("Updating old file in MongoDB")
+        # Delete the old file from GridFS if it exists
+        old_file = fs.find_one({"filename": "old.gz"})
+        if old_file:
+            fs.delete(old_file._id)
+
+        # Store the compressed file in MongoDB
+        with open(compressed_file_path, "rb") as file:
+            file_id = fs.put(file, filename="old.gz", contentType="application/gzip")
+
+        print("Compressed file stored with ID:", file_id)
+    else:
+        print("No new reviews found. Old file in MongoDB is up to date")
+    # Clean up: You can delete the compressed file if you no longer need it locally.
+    os.remove(compressed_file_path)
+    os.remove(file_path)
 
 
 if __name__ == "__main__":
@@ -107,8 +161,12 @@ if __name__ == "__main__":
         with open("./config.json") as config_file:
             config = json.load(config_file)
             URI = config["URI"]
-    except FileNotFoundError as error:
+    except FileNotFoundError:
         URI = os.environ.get("URI")
+    client = MongoClient(URI)
+    db = client['UCR']
+    review_collection = db['reviews']
+    course_collection = db['courses']
     download_new_sheet()
     compare_files()
     print("Script has finished succesfully")
--- services/scheduled_tasks.js
+++ services/scheduled_tasks.js
@@ -1,9 +1,10 @@
 const cron = require('node-cron');
 const Review = require("../models/reviewModel");
 const ps = require("../services/puppeteer_service.js");
+const { exec } = require("child_process");
+
 // 1) SCHEDULED TASKS
 // 1.1) FILL FORM FOR ALL REVIEW NOT SUBMITTED EVERY DAY AT 00:00
-
 cron.schedule('0 0 * * *', async () => {
   console.log('\x1b[33m%s\x1b[0m', "**Cron Job Started**")
   console.log('\x1b[33m%s\x1b[0m', "**Querying for reviews to fill form...**")
@@ -22,4 +23,21 @@ cron.schedule('0 0 * * *', async () => {
     });  
     console.log('\x1b[33m%s\x1b[0m', "**Done filling forms**")
   });
+});
+
+// 1.2) RUN PYTHON SCRIPT TO UPDATE DATABASE ONCE A WEEK AT 00:00 ON SUNDAY
+cron.schedule('0 0 * * 0', async () => {
+  console.log('\x1b[33m%s\x1b[0m', "**Cron Job Started**")
+  console.log('\x1b[33m%s\x1b[0m', "**Running python script to update database...**")
+  exec("python3 ../server_scripts/GSDiffing.py", (error, stdout, stderr) => {
+    if (error) {
+      console.log('\x1b[1m\x1b[41m%s\x1b[0m', "SOMETHING WENT WRONG!!!")
+      console.log('\x1b[33m%s\x1b[0m', "Will try again next time...")
+      console.log(error)
+      return;
+    }
+    console.log(stdout);
+    console.log(stderr);
+  });
+  console.log('\x1b[33m%s\x1b[0m', "**Done updating database**")
 });
